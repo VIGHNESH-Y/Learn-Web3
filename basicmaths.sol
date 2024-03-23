ad62b752493f66aa40b31ece93e83a205cf036d6
Vighnesh Y <80388978+VIGHNESH-Y@users.noreply.github.com>
2024-03-23 14:09:44 +0530

    Update

---
+++ basicmaths.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifi
+pragma solidity ^0.8.0;
+
+contract MathOperations {
+    
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+    
+    function subtract(uint256 a, uint256 b) public pure returns (uint256) {
+        require(a >= b, "Subtraction result would be negative");
+        return a - b;
+    }
+}
